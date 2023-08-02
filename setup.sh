05d088127d3fa86f4d68a7b1a6ecc3cd63e7a67b
Logical <133435955+Logic067@users.noreply.github.com>
2023-08-03 00:32:28 +0200

    Update setup.sh
    Fixed error

--- setup.sh
+++ setup.sh
@@ -11,12 +11,26 @@ exit 1
 fi
 exit 0
 fi
+if [ "$warning_bedit_installed" = "y" ]; then
+sudo rm -rf "/snap/boot_editor_v13r3"
+sudo cp -r -f * "/snap/boot_editor_v13r3" 
+sudo cp -r "bedit" "/bin/bedit"
+if [ $? -ne 0 ]; then
+echo "Error detected try agaian"
+exit 1
 fi
-if [ "$warning_bedit_installed" = "no" ]; then
-echo "Bye bye"
 exit 0
 fi
-sudo cp -r * "/snap/boot_editor_v13r3" 
+if [ "$warning_bedit_installed" == "n" ]; then
+echo ""
+exit 0
+fi
+if [ "$warning_bedit_installed" == "no" ]; then
+echo ""
+exit 0
+fi
+cd ..
+sudo cp -r "bedit" "/snap/boot_editor_v13r3" 
 sudo cp -r "bedit" "/bin/bedit"        
 if [ $? -ne 0 ]; then
 echo "Error detected try agaian"
