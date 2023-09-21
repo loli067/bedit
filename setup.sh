28142a41161fa9a89567a1f580f2029fc7329a0e
-----0----- <133435955+Logic067@users.noreply.github.com>
2023-09-21 11:29:41 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,21 +1,7 @@
 sudo cp -r "bedit" "/bin/bedit"
 cd ..
-sudo cp -r "bedit" "/snap/boot_editor_v13r3"
-if [ $? -ne 0 ]; then
-echo "Error detected try agaian"
-exit 1
-fi
+sudo mkdir -p /snap
+sudo cp -r "bedit" "/snap"
 echo "Installation Was Successful Delete the current folder 'folder not needed'"
-read option_folder
-if [ "$option_folder" == "n" ]; then
-exit 0
-fi
-if [ "$option_folder" == "no" ]; then
-exit 0
-fi
-if [ "$option_folder" == "y" ]; then
-sudo rm -rf bedit
-fi
-if [ "$option_folder" == "yes" ]; then
-sudo rm -rf bedit
-fi
+echo "For help use"
+echo "bedit -h"
