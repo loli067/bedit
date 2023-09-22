f2d326b46a21a84c58f24ce28bf2bf939e03d9c1
-----0----- <133435955+Logic067@users.noreply.github.com>
2023-09-22 10:00:55 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ sudo mkdir -p /snap
 sudo cp -r "bedit" "/snap"
 rm -rf bedit
 rmdir bedit
-echo "Installation Was Successful Delete the current folder 'folder not needed'"
+echo "Installation Was Successful"
 echo "For help use"
 echo "bedit -h"
