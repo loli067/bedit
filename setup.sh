e1a09df1adf4ef222b8aaa6329168764f32806c7
-----0----- <133435955+Logic067@users.noreply.github.com>
2023-11-07 13:39:48 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,14 @@ cd ..
 sudo mkdir -p /snap
 sudo cp -r "bedit" "/lib"
 rm -rf bedit
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "if you get error message:"
+echo "sh: 0: getcwd() failed: No such file or directory"
+echo "type cd to fix or open a new terminal"
 echo "Installation Was Successful"
 echo "For help use"
 echo "bedit -h"
